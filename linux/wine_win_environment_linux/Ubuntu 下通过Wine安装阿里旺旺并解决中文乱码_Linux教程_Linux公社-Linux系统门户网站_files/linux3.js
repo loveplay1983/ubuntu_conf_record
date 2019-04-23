9def4d7b255afcfc242668301e742c37025337db
Michael Xuan <michaelxuan@hotmail.com>
2019-04-23 11:48:21 +0800

    first push

---
+++ linux/wine_win_environment_linux/Ubuntu 下通过Wine安装阿里旺旺并解决中文乱码_Linux教程_Linux公社-Linux系统门户网站_files/linux3.js
@@ -0,0 +1,9 @@
+﻿document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+document.writeln("<!-- 728x90, 创建于 09-12-17 -->");
+document.writeln("<ins class=\'adsbygoogle\'");
+document.writeln("     style=\'display:inline-block;width:728px;height:90px\'");
+document.writeln("     data-ad-client=\'ca-pub-5195587195407606\'");
+document.writeln("     data-ad-slot=\'6090386898\'></ins>");
+document.writeln("<script>");
+document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+document.writeln("</script>");
